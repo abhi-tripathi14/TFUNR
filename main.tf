b16c6843d4c19a7b49ce69d432f636a8437ddb55
abhi-tripathi14 <abhi.tri87@gmail.com>
2022-12-26 14:27:26 +0530

    Updated aws security group details

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-ami = "ami-0fb653ca2d3203ac1" 
-instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.SecGroup.id]
 
-tags = {
-  "Name" = "Terraform-Abhinav"
+  user_data                   = <<-EOF
+#!/bin/bash
+echo"Hello, World" >index.html
+nohub busybox httpd -f -p 8080 &
+EOF
+  user_data_replace_on_change = true
+  tags = {
+    "Name" = "Terraform-Abhinav"
+  }
 }
+
+resource "aws_security_group" "SecGroup" {
+  name = "Terraform-Abhinav-SecGroup"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
